f1986c6fee1e50412fc9287af6ecad62c4f52be1
Jae Yong Kim <kim900180@gmail.com>
2018-07-28 07:30:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,14 @@
 # Write your code here.
 def line(array)
+  string = ""
+  counter = 0
   if array == []
     puts "The line is currently empty."
+  if array != []
+    while counter < array.size
+      string << "#{counter}. #{array[counter]}"
+      counter += 1
+    end
+    puts "The line is currently: " + string
   end
 end
