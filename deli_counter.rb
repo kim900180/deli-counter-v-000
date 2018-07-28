82cd5c8c5e161377a0ca88519830391661499455
Jae Yong Kim <kim900180@gmail.com>
2018-07-28 07:38:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,6 @@ def now_serving(array)
     puts "Currently serving #{array[0]}."
     array.shift
   else
-    puts "There is nobody waiting to be served!."
+    puts "There is nobody waiting to be served!"
   end
 end
