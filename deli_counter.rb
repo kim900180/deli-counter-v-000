97ec5a7aae534c2617ae1bbdff689af5f65733ea
Jae Yong Kim <kim900180@gmail.com>
2018-07-28 07:36:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,6 +19,6 @@ def take_a_number(array, name)
 end
 
 def now_serving(array)
+  array.size > 0 ? "Currently serving #{array[0]}." : "There is nobdy waiting to be served."
   array.shift
-  puts "Currently serving #{array[0]}."
 end
